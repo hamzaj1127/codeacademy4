b81b2f20aac5142dee97d44354adbdc3236825a7
mizo8 <mizo8@DESKTOP-4BG94LB>
2022-10-21 23:33:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_checker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username, password;
+            int ctr = 0;
+            Console.Write("\n\nCheck username and password :\n");
+            Console.Write("N.B. : Default username and password is :abc and efg\n");
+            Console.Write("------------------------------------------------------\n");
+
+            do
+            {
+                Console.Write("Input a username: ");
+                username = Console.ReadLine();
+
+                Console.Write("Input a password: ");
+                password = Console.ReadLine();
+
+                if (username != "abc" || password != "efg")
+                    ctr++;
+                else
+                    ctr = 1;
+
+            }
+            while ((username != "abc" || password != "efg") && (ctr != 3));
+
+            if (ctr == 3)
+                Console.Write("\nLogin attempted three or more times. You have been locked out" +
+                    "!\n\n");
+            else
+                Console.Write("\nThe password was entered successfully!\n\n");
+        }
+    }
+}
